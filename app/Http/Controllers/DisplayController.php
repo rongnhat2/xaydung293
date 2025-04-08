95bf958a197f0afa8f30d5a58c2247c0fbfb58ad
rongnhat2 <rongnhat2@gmail.com>
2025-04-08 11:31:55 +0700

    create base

--- app/Http/Controllers/DisplayController.php
+++ app/Http/Controllers/DisplayController.php
@@ -12,4 +12,29 @@ class DisplayController extends Controller
     public function services(){
         return view('services');
     }
+    public function sitemap()
+    {
+        // Array to store URLs
+        $urls = [];
+
+        // Add static pages
+        $urls[] = [
+            'loc' => URL::to('/'),
+            'lastmod' => Carbon::now()->toAtomString(),
+            'changefreq' => 'daily',
+            'priority' => '1.0'
+        ];
+
+        $urls[] = [
+            'loc' => URL::to('/dich-vu'),
+            'lastmod' => Carbon::now()->toAtomString(),
+            'changefreq' => 'monthly',
+            'priority' => '0.8'
+        ]; 
+        // Generate XML
+        $xml = $this->generateSitemap($urls);
+
+        return response($xml, 200)
+            ->header('Content-Type', 'application/xml');
+    }
 }
