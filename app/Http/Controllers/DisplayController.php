3aa17af798aa5571c340a3116a5a74e50b024075
rongnhat2 <rongnhat2@gmail.com>
2025-04-08 11:34:42 +0700

    create base

--- app/Http/Controllers/DisplayController.php
+++ app/Http/Controllers/DisplayController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+use Carbon\Carbon;
+use Session;
+use Hash;
+use DB;
 
 class DisplayController extends Controller
 {
